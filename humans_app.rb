ea592909437e23fa7bb23ffc60bd16ec627f63ef
Peter Hellberg <peter@c7.se>
2012-10-28 10:03:03 +0100

    Disable the Redis cache by using the query argument 'cache_disabled'

--- humans_app.rb
+++ humans_app.rb
@@ -21,10 +21,20 @@ class HumansApp < Sinatra::Base
 
     content_type :json
 
-    RedisCache.get("humans:#{params[:host]}", 300) {
+    readthrough_cache do
       d = HumansTxt.download_and_parse(params[:host], params[:use_ssl])
       params[:pretty].to_s == 'true' ? JSON.pretty_generate(d) : JSON.dump(d)
-    }
+    end
+  end
+
+  private
+
+  def readthrough_cache(&block)
+    if params.has_key? 'cache_disabled'
+      yield
+    else
+      RedisCache.get("humans:#{params[:host]}", 300) { yield }
+    end
   end
 
   def validate_hostname
